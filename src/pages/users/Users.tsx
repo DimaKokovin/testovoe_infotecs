4a418e14e08b62468d5cd87fb4da84086807a566
Dmitry <kokovin.02@inbox.ru>
2026-01-19 21:38:37 +0300

    add  users page

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,7 +1,81 @@
-export const Users = ( ) => {
-    return (
-        <>
-            users
-        </>
-    )
+
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import dayjs from "dayjs";
+import {useNavigate} from "react-router-dom";
+import s from './Users.module.css'
+
+interface IUser {
+    id: string;
+    name: string;
+    avatar: string;
+    createdAt: string;
 }
+
+export const Users = () => {
+
+    const navigate = useNavigate();
+
+
+
+
+    const { data: users, isLoading, error } = useQuery({queryKey: ["users"], queryFn: async () => {
+
+            const response = await axios.get("https://696e4c6ed7bacd2dd71660e1.mockapi.io/users");
+            return response.data;
+        }
+    });
+
+    const handleLogout = () => {
+
+        localStorage.removeItem("token");
+
+        navigate("/login")
+    };
+
+    const handleCreateUser = () => {
+
+    };
+
+    const handleEditUser = (_user: IUser) => {
+
+    };
+
+    if (isLoading) return <div className={s.usersPage}>Загрузка...</div>;
+    if (error) return <div className={s.usersPage}>Ошибка загрузки пользователей</div>;
+
+    return (
+        <div className={s.usersPage}>
+            <h1>Пользователи</h1>
+
+            <div style={{ display: "flex", justifyContent: "flex-end", marginBottom: "20px" }}>
+                <button onClick={handleLogout}>Выход</button>
+            </div>
+
+            <ul className={s.usersList}>
+                {users?.map((user:IUser) => (
+                    <li key={user.id}>
+                        <img
+                            src={user.avatar}
+                            alt={user.name}
+                            onClick={() => handleEditUser(user)}
+                        />
+                        <div className={s.userInfo} onClick={() => handleEditUser(user)}>
+                            <span>{user.name}</span>
+                            <span>Зарегистрирован: {dayjs(user.createdAt).format("DD.MM.YYYY")}</span>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+
+            <div style={{ marginTop: "20px" }}>
+                <button onClick={handleCreateUser}>Создать пользователя</button>
+            </div>
+
+
+
+
+
+        </div>
+    )
+};
